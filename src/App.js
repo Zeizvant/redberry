7f4118203030d5ceeca871fa205b3dd2a6e3994d
admin <admin@admins-MacBook-Pro-2.local>
2023-02-03 20:48:21 +0400

    created landing page and it's design closes #2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Landing} from './components/Landing/Landing';
 function App() {
   return (
     <div className="App">
+      <Landing />
     </div>
   );
 }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,17 @@
 import React from "react"
 import './index.css'
+import redberry from '../../images/redberry-name-logo.png'
+import logo from '../../images/transparent-logo.png'
+
 export const Landing = () => {
     return (
-        <div>landing component</div>
+        <div className='landing-page'>
+            <div className="redberry-logo">
+                <img src={redberry} alt='redberry logo'/>
+            </div>
+            <div className="landing-main">
+                <button className='landing-button'>ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ</button>
+            </div>
+        </div>
     )
 }
