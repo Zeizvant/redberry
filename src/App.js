cbfd24646d388b625ca27dc31a350682e1b6abaa
admin <admin@admins-MacBook-Pro-2.local>
2023-02-04 17:51:50 +0400

    added Experience component structure and design closes #5

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import {Landing} from './components/Landing/Landing';
 import { PersonInfo } from './components/PersonInfo/PersonInfo'
 import { Info } from './components/Info/Info'
+import { Experience } from './components/Experience/Experience'
 
 
 function App() {
   return (
     <div className="App">
-      <PersonInfo />
+      <Experience />
       <Info />
     </div>
   );
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,7 +1,50 @@
 import React from "react"
 import './index.css'
+import backSign from '../../images/back-sign.png'
+
 export const Experience = () => {
     return (
-        <div>experience form component</div>
+        <div className="form experiecne-form">
+            <div className='back-sign'>
+                <img src={backSign} alt='back sign button'/>
+            </div>
+            <form>
+                <div className='form-header'>
+                    <span>ᲒᲐᲛᲝᲪᲓᲘᲚᲔᲑᲐ</span>
+                    <span className='page-number'>2/3</span>
+                </div>
+                <div className="text-input long">
+                    <p className="label">თანამდებობა</p>
+                    <input type='text' placeholder="დეველოპერი, დიზაინერი, ა.შ." />
+                    <p className='input-validation'>მინიმუმ 2 სიმბოლო</p>
+                </div>
+                <div className="text-input long">
+                    <p className="label">დამსაქმებელი</p>
+                    <input type='text' placeholder="დამსაქმებელი" />
+                    <p className='input-validation'>მინიმუმ 2 სიმბოლო</p>
+                </div>
+                <div className='two-input'>
+                    <div className="text-input">
+                        <p className="label">დაწყების რიცხვი</p>
+                        <input type='date'/>
+                    </div>
+                    <div className='text-input'>
+                        <p className="label">დამთავრების რიცხვი</p>
+                        <input type='date' />
+                    </div>
+                </div>
+                <div className="textarea-div">
+                    <p className='label'>აღწერა</p>
+                    <textarea placeholder='როლი თანამდებობაზე და ზოგადი აღწერა'></textarea>
+                </div>
+                <div>
+                    <button className="add-button">მეტი გამოცდილების დამატება</button>
+                </div>
+                <div className="navigation-buttons">
+                    <button className='form-buttons'>უკან</button>
+                    <button className='form-buttons'>შემდეგი</button>
+                </div>
+            </form>
+        </div>
     )
 }
