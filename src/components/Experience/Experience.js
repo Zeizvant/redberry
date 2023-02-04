a0e23caa8fa675ebb577375407d9a84716cc8ba3
admin <admin@admins-MacBook-Pro-2.local>
2023-02-04 20:26:24 +0400

    added state to Experience component

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,8 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import './index.css'
 import backSign from '../../images/back-sign.png'
 
 export const Experience = () => {
+
+    const [position, setPosition] = useState([''])
+    const [employer, setEmployer] = useState([''])
+    const [startDate, setStartDate] = useState([''])
+    const [dueDate, setDueDate] = useState([''])
+    const [experienceDescription, setExperienceDescription] = useState([])
+    const form = 0
+
+
     return (
         <div className="form experiecne-form">
             <div className='back-sign'>
@@ -15,27 +24,39 @@ export const Experience = () => {
                 </div>
                 <div className="text-input long">
                     <p className="label">თანამდებობა</p>
-                    <input type='text' placeholder="დეველოპერი, დიზაინერი, ა.შ." />
+                    <input type='text' placeholder="დეველოპერი, დიზაინერი, ა.შ." value={position[0]} onChange={(event) => {
+                        setPosition([event.target.value])
+                    }}/>
                     <p className='input-validation'>მინიმუმ 2 სიმბოლო</p>
                 </div>
                 <div className="text-input long">
                     <p className="label">დამსაქმებელი</p>
-                    <input type='text' placeholder="დამსაქმებელი" />
+                    <input type='text' placeholder="დამსაქმებელი" value={employer[0]} onChange={(event) => {
+                        setEmployer([event.target.value])
+                    }}/>
                     <p className='input-validation'>მინიმუმ 2 სიმბოლო</p>
                 </div>
                 <div className='two-input'>
                     <div className="text-input">
                         <p className="label">დაწყების რიცხვი</p>
-                        <input type='date'/>
+                        <input type='date' value={startDate[0]} onChange={(event) => {
+                            setStartDate([event.target.value])
+                        }}/>
                     </div>
                     <div className='text-input'>
                         <p className="label">დამთავრების რიცხვი</p>
-                        <input type='date' />
+                        <input type='date' value={dueDate[0]} onChange={(event) => {
+                            setDueDate([event.target.value])
+                        }}/>
                     </div>
                 </div>
                 <div className="textarea-div">
                     <p className='label'>აღწერა</p>
-                    <textarea placeholder='როლი თანამდებობაზე და ზოგადი აღწერა'></textarea>
+                    <textarea placeholder='როლი თანამდებობაზე და ზოგადი აღწერა' value={experienceDescription[0]} onChange={(event) => {
+                        setExperienceDescription([event.target.value])
+                    }}>
+
+                    </textarea>
                 </div>
                 <div>
                     <button className="add-button">მეტი გამოცდილების დამატება</button>
